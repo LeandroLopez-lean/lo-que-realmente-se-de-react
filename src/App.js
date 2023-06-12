b463d7bf25542bffeeba20ac07b2e48207392a40
Leandro <leandrolopez131982@gmail.com>
2023-06-12 19:22:39 -0300

    le agrege comentarios sobre hashrouter y navlink

--- src/App.js
+++ src/App.js
@@ -20,3 +20,4 @@ function App() {
 }
 
 export default App;
+/*HashRouter en React es un tipo de enrutador proporcionado por la biblioteca React Router. Este enrutador utiliza el hash en la URL para manejar la navegación y el enrutamiento interno en una SPA. Permite definir diferentes rutas en la aplicación y asociar componentes con esas rutas. Cuando el usuario navega a una ruta específica, el componente correspondiente se renderiza en la interfaz de usuario sin enviar una solicitud al servidor. */
--- src/Menu.js
+++ src/Menu.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+// con las etiquetas NavLink importadas desde react-router-dom a diferncia de las etiquetas <A> 
+//el navegador no necesita renderizar todo el html
 
 function Menu(){
     return(
-        <h1>Bienvenidos</h1>
-      
+        <header>
+            <h1>Bienvenidos</h1>
+            <nav>
+               <ul>
+                 <li>
+                   <NavLink
+                   style={({ isActive}) => ({color: isActive ? 'red' : 'blue'})}
+                   to="/">Galeria</NavLink>
+                 </li>
+                 <li>
+                   <NavLink 
+                   style={({ isActive}) => ({color: isActive ? 'red' : 'blue'})}
+                   to="/Info">Info</NavLink>
+                 </li>
+                </ul>
+            </nav>
+      </header>
     );
 }
 
